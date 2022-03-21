640650341d4ef1697de2a40fba6cbe02514a31ea
email <kibetdavidro@gmail.com>
2022-03-21 22:52:18 +0300

    add category model class

---
+++ neighbour_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime as dt
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-updated_at','-created_at']
+
+    
